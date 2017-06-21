0912246b50e33c643c21f1e01f705bebc1211825
subravte <tejaswini.subraveti@gmail.com  git config --global user.name subravte>
2017-06-22 04:45:47 +1000

    moved methods from view controller to datamanager. full tile mode activated. read me and references added

---
+++ NetFlixS/FullTileViewController.h
@@ -0,0 +1,13 @@
+//
+//  FullTileViewController.h
+//  NetFlixS
+//
+//  Created by TEJASWINI SUBRAVETI on 22/6/17.
+//  Copyright © 2017 TEJASWINI SUBRAVETI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FullTileViewController : UIViewController
+
+@end
---
+++ NetFlixS/FullTileViewController.m
@@ -0,0 +1,37 @@
+//
+//  FullTileViewController.m
+//  NetFlixS
+//
+//  Created by TEJASWINI SUBRAVETI on 22/6/17.
+//  Copyright © 2017 TEJASWINI SUBRAVETI. All rights reserved.
+//
+
+#import "FullTileViewController.h"
+
+@interface FullTileViewController ()
+
+@end
+
+@implementation FullTileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
